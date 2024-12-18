6c04ed5e6c826f8e7cc91f46b1c7b30ffd2fd126
David Arnold <10138997+djaboxx@users.noreply.github.com>
2024-12-18 14:15:34 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -294,7 +294,7 @@ data "aws_iam_policy_document" "this" {
       }
 
       actions   = ["es:*"]
-      resources = ["arn:aws:es:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:domain/${var.domain_name}/*"]
+      resources = ["arn:${local.partition}:es:${local.region}:${local.account_id}:domain/${var.domain_name}/*"]
     }
   }
 
